0dcefc081cdb0db7870ef9f8a35e638a11a32a3d
zhangyanbin <zhangyanbin@300.cn>
2018-09-01 19:55:29 +0800

    1.优化游戏列表接口（对需要根据游戏进行筛选的功能，修改了返回值） 2.优化了评论列表接口（利用left join 将游戏名称添加到了返回值中）

--- app/service/game.js
+++ app/service/game.js
@@ -1,41 +1,49 @@
-const Service = require('egg').Service;
+const Service = require("egg").Service;
 
 class GameService extends Service {
-	constructor(ctx) {
-		super(ctx);
-		// this.root = 'https://cnodejs.org/api/v1';
-	}
+  constructor(ctx) {
+    super(ctx);
+    // this.root = 'https://cnodejs.org/api/v1';
+  }
 
-	async index(payload) {
-		// 查询所有
-		try {
-			const result = await this.app.mysql.select('game', {
-				where: {
-					is_sold: payload.isSold //是否发售
-				},
-				columns: ['id', 'game_name', 'game_cover', 'game_desc', 'sale_date']
-			});
-			console.log('result: ', result);
-			return result;
-		} catch (err) {
-			this.logger.error(err);
-			return err.code;
-		}
-	}
+  async index(payload) {
+    console.log("payload: ", payload);
+    // 查询所有
+    try {
+      let condition = {};
+      let columns = ["id", "game_name", "game_cover", "game_desc", "sale_date"];
+      if (payload.isSold) {
+        Object.assign(condition, { is_sold: payload.isSold });
+      }
+      if (payload.isFilter) {
+        //如果是筛选游戏的话，不用返回那么字段
+        columns = ["id", "game_name"];
+      }
+      const result = await this.app.mysql.select("game", {
+        where: condition,
+        columns: columns
+      });
+      console.log("result: ", result);
+      return result;
+    } catch (err) {
+      this.logger.error(err);
+      return err.code;
+    }
+  }
 
-	async show(id) {
-		// 查询单条
-		try {
-			const result = await this.app.mysql.get('game', {
-				id: id
-			});
-			console.log('result: ', result);
-			return result;
-		} catch (err) {
-			this.logger.error(err);
-			return err.code;
-		}
-	}
+  async show(id) {
+    // 查询单条
+    try {
+      const result = await this.app.mysql.get("game", {
+        id: id
+      });
+      console.log("result: ", result);
+      return result;
+    } catch (err) {
+      this.logger.error(err);
+      return err.code;
+    }
+  }
 }
 
 module.exports = GameService;
