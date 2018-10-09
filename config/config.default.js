6021ef8e252a11467e9a39386432585fc6f2e073
zhangyanbin <zhangyanbin@300.cn>
2018-10-09 08:42:01 +0800

    1.优化游戏资讯接口 	2.修改bodyParser接受post数据的大小限制

--- config/config.default.js
+++ config/config.default.js
@@ -1,76 +1,80 @@
-'use strict';
+"use strict";
 
 module.exports = appInfo => {
-	const config = exports = {};
+  const config = (exports = {});
 
-	exports.cluster = {
-		listen: {
-			port: 3000,
-			hostname: '127.0.0.1',
-			// hostname: '192.168.31.66', //测试
-			// hostname: '172.22.152.111',//测试
+  exports.cluster = {
+    listen: {
+      port: 3000,
+      hostname: "127.0.0.1"
+      // hostname: '192.168.31.66', //测试
+      // hostname: '172.22.152.111',//测试
 
-			// path: '/var/run/egg.sock',
-		}
-	}
+      // path: '/var/run/egg.sock',
+    }
+  };
 
-	// 关闭csrf安全机制(前后端分离作用不大)
-	exports.security = {
-		csrf: false,
+  // 关闭csrf安全机制(前后端分离作用不大)
+  exports.security = {
+    csrf: false,
 
-		// 跨域白名单
-		domainWhiteList: ['http://localhost:9000']
-	};
+    // 跨域白名单
+    domainWhiteList: ["http://localhost:9000"]
+  };
 
-	// 跨域可支持的方法
-	config.cors = {
-		// 所有域名可以访问
-		origin: '*',
-		allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
-		credentials: true
-	};
+  // 跨域可支持的方法
+  config.cors = {
+    // 所有域名可以访问
+    origin: "*",
+    allowMethods: "GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS",
+    credentials: true
+  };
 
-	// use for cookie sign key, should change to your own and keep security
-	config.keys = appInfo.name + '_1533605945307_6780';
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + "_1533605945307_6780";
 
-	// add your config here
-	config.middleware = [];
+  // add your config here
+  config.middleware = [];
 
+  //mysql数据库基本信息
+  exports.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: "localhost",
+      // 端口号
+      port: "3306",
+      // 用户名
+      user: "root",
+      // 密码
+      password: "19900121",
+      // 数据库名
+      database: "demo"
 
-	//mysql数据库基本信息
-	exports.mysql = {
-		// 单数据库信息配置
-		client: {
-			// host
-			host: 'localhost',
-			// 端口号
-			port: '3306',
-			// 用户名
-			user: 'root',
-			// 密码
-			password: '19900121',
-			// 数据库名
-			database: 'demo',
+      // charset : 'utf8mb4' 识别emoji
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false
+  };
 
-			// charset : 'utf8mb4' 识别emoji
-		},
-		// 是否加载到 app 上，默认开启
-		app: true,
-		// 是否加载到 agent 上，默认关闭
-		agent: false,
-	};
+  //jwt相关配置
+  exports.jwt = {
+    secret: "zhangyanbin"
+  };
 
-	//jwt相关配置
-	exports.jwt = {
-		secret: "zhangyanbin"
-	};
+  //jwt相关配置
+  config.jwt = {
+    secret: "Great4-M",
+    enable: true, // default is false
+    match: ["/manage"] // 需要认证的接口
+  };
 
-	//jwt相关配置
-	config.jwt = {
-		secret: 'Great4-M',
-		enable: true, // default is false
-		match: ['/manage'], // 需要认证的接口
-	}
+  exports.bodyParser = {
+    jsonLimit: "10mb",
+    formLimit: "10mb"
+  };
 
-	return config;
+  return config;
 };
